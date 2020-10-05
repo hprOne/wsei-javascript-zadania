5cb405ae28ffb666499b5a0f78db67eb698764cc
hprOne <bartek.laka@gmail.com>
2020-10-05 11:29:57 +0200

    2. Zmienne i stałe

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,29 @@
+//Zadanie 1:
+
+const	num = 1,				//liczba
+		str = '2',				//string
+		combo = num + str		//liczba + string
+		bool = true,			//wartosc logiczna
+		nothing = null;			//wartosc specjalna
+
+console.log(num);				//Wyswietlenia w konsoli dla 
+console.log(str);				//poszczegolnych typów zmiennych
+console.log(combo);
+console.log(bool);
+console.log(nothing);
+
+//Zadanie 2:
+
+let	num = 1,
+	num1 = 2,
+	sum = 0;					
+
+sum = num + num1;
+
+console.log(sum);
+
+//Zadanie 3:
+
+let num;						//brak przypisanej wartosci zmiennej
+
+console.log(num);
