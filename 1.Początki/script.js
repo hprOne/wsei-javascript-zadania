d288773161ed247567b4d2c47a45ce7771361dfa
hprOne <bartek.laka@gmail.com>
2020-10-05 11:16:47 +0200

    1. Początki

---
+++ 1.Początki/script.js
@@ -0,0 +1,13 @@
+//Zadanie 1:
+
+console.log("Bart³omiej £¹ka")		//Bart³omiej £¹ka
+
+//Zadanie 2:
+
+console.log(12+32);					//44
+console.log(52/16);					//3,25
+console.log(5*2);					//10
+
+//Zadanie 3:
+
+console.log("Hello World!")			//Hello World!
