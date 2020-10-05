b229038fe8614db419a4315cb1247452a5b00f04
Bartłomiej Łąka <59585090+hprOne@users.noreply.github.com>
2020-10-05 11:41:39 +0200

    1. Początki

--- 1.Początki/script.js
+++ 1.Początki/script.js
@@ -1,13 +1,13 @@
 //Zadanie 1:
 
-console.log("Bartlomiej Laka")		//Bartlomiej Laka
+console.log("Bartłomiej Łąka")		//Bartłomiej Łąka
 
 //Zadanie 2:
 
-console.log(12+32);					//44
-console.log(52/16);					//3,25
-console.log(5*2);					//10
+console.log(12+32);					      //44
+console.log(52/16);					      //3,25
+console.log(5*2);					        //10
 
 //Zadanie 3:
 
-console.log("Hello World!")			//Hello World!
+console.log("Hello World!")			  //Hello World!
