9b9771e7a90286d4c52ce94a82e30651ba755e49
hprOne <bartek.laka@gmail.com>
2021-01-04 21:30:49 +0100

    5. Funkcje czasu (COMPLETE)

---
+++ 5. Funkcje czasu/script.js
@@ -0,0 +1,30 @@
+//Zadanie 1:
+
+let time = 0;
+
+const interval = setInterval(() => {
+  time += 1;
+  console.log(`Czeœæ po raz ${time}`);
+  if(time>=15)
+    {
+      clearInterval(interval);
+    }
+}, 3000);
+
+//Zadanie 2:
+
+var table=[1,2,3,4,5,6,7,8,9,10]
+
+setTimeout(()=>{
+  for(let i=0; i<table.length; i++) {
+    console.log(table[i]);
+  }
+  let i=0;
+  var interval = setInterval(()=> {
+    console.log(table[i]);
+    i++
+    if(i>table.length){
+      clearInterval(interval);
+    }
+  }, 2000)
+}, 3000)
